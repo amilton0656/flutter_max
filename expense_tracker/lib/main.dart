33e759f93cea15e3072ca0db05b284c30487bb96
amilton0656 <amilton0656@gmail.com>
2023-07-26 17:49:24 -0300

    aaa

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widgets/expenses.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses()
+    ),
+  );
+}
+
+//flutter pub add uuid
+//flutter pub add intl
---
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print('Valor inserido : ' + _titleController.text);
+                  },
+                  child: Text('Save Expense'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
